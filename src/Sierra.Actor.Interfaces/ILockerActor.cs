ac6044c187273c2c12017d652db9e54c51af701d
djfr <davidferreirarodrigues@gmail.com>
2017-12-08 16:40:16 +0000

    Initial draft of the locker actor

---
+++ src/Sierra.Actor.Interfaces/ILockerActor.cs
@@ -0,0 +1,16 @@
+﻿namespace Sierra.Actor.Interfaces
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceFabric.Actors;
+
+    /// <summary>
+    /// Defines a common contract for the LockerActor.
+    /// </summary>
+    public interface ILockerActor : IActor
+    {
+        Task QueueAdd(Type type, ActorId id);
+
+        Task QueueRemove(Type type, ActorId id);
+    }
+}
---
+++ src/Sierra.Actor/LockerActor.cs
@@ -0,0 +1,26 @@
+﻿namespace Sierra.Actor
+{
+    using System;
+    using System.Threading.Tasks;
+    using Interfaces;
+    using Microsoft.ServiceFabric.Actors;
+    using Microsoft.ServiceFabric.Actors.Runtime;
+
+    public class LockerActor : Actor, ILockerActor
+    {
+        public LockerActor(ActorService actorService, ActorId actorId)
+            : base(actorService, actorId)
+        {
+        }
+
+        public Task QueueAdd(Type type, ActorId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task QueueRemove(Type type, ActorId id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
