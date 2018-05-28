930ae051dd8eb28d5573e1e3669776dbb435267a
Vaclav Haken <vaclav@vaclavkabat.net>
2018-05-28 15:26:01 +0100

    access token in

--- src/Tests/Sierra.Api.Tests/ForkTests.cs
+++ src/Tests/Sierra.Api.Tests/ForkTests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Eshopworld.DevOps;
 using Eshopworld.Tests.Core;
 using FluentAssertions;
+using IdentityModel.Client;
 using Sierra.Common;
 using Xunit;
 
@@ -24,27 +25,32 @@ public class ForkTests
         HttpClient client = new HttpClient();
         var suffix = Guid.NewGuid().ToString();
 
+        //obtain access token
+        var stsAccessToken = await ObtainSTSAccessToken();
+        client.SetBearerToken(stsAccessToken);
+
         //issue fork request
         var respo = await client.PostAsync(Settings.ApiUrl,
             new StringContent(
                 $"{{\"sourceRepositoryName\": \"ForkIntTestSourceRepo\",\"forkSuffix\": \"{suffix}\"}}", Encoding.UTF8, "application/json"));
+
         respo.StatusCode.Should().Be(HttpStatusCode.OK);
 
         //list repos
         var vstsConfig = await ForkUtilities.LoadVstsConfiguration(Settings.KeyVaultUrl, Settings.KeyVaultClientId,
             Settings.KeyVaultClientSecret);
 
-        var accessToken = await ForkUtilities.ObtainVstsAccessToken(Settings.KeyVaultUrl, Settings.KeyVaultClientId,
+        var vstsAccessToken = await ForkUtilities.ObtainVstsAccessToken(Settings.KeyVaultUrl, Settings.KeyVaultClientId,
             Settings.KeyVaultClientSecret, vstsConfig);
 
         var repoName = $"ForkIntTestSourceRepo-{suffix}";
 
-        var repos = await ForkUtilities.ListRepos(accessToken, vstsConfig);
+        var repos = await ForkUtilities.ListRepos(vstsAccessToken, vstsConfig);
         var repo = repos.FirstOrDefault(i => i.Name == repoName);
         repo.Should().NotBeNull();
 
         //delete the repo
-        await ForkUtilities.DeleteRepo(accessToken, repo.Id, vstsConfig);
+        await ForkUtilities.DeleteRepo(vstsAccessToken, repo.Id, vstsConfig);
     }
 
     private async Task SetConfiguration()
@@ -56,11 +62,25 @@ public class ForkTests
             ApiUrl = config["TestConfig:ApiUrl"],
             KeyVaultUrl = config["TestConfig:KeyVaultUrl"],
             KeyVaultClientSecret = config["TestConfig:KeyVaultClientSecret"],
-            KeyVaultClientId = config["TestConfig:KeyVaultClientId"]
+            KeyVaultClientId = config["TestConfig:KeyVaultClientId"],
+            STSAuthority = config["TestConfig:STSAuthority"],
+            STSClientSecret = config["TestConfig:STSClientSecret"],
+            STSClientId = config["TestConfig:STSClientId"],
+            STSScope = config["TestConfig:STSScope"]
         };
 
         VstsConfig = await ForkUtilities.LoadVstsConfiguration(Settings.KeyVaultUrl, Settings.KeyVaultClientId,
             Settings.KeyVaultClientSecret);
+    }
+
+    // ReSharper disable once InconsistentNaming
+    private async Task<string> ObtainSTSAccessToken()
+    {
+        var discovery = await DiscoveryClient.GetAsync(Settings.STSAuthority);
+        var client = new TokenClient(discovery.TokenEndpoint, Settings.STSClientId, Settings.STSClientSecret);
+
+        var tokenResponse = await client.RequestClientCredentialsAsync(Settings.STSScope);
+        return tokenResponse.AccessToken;
     }
 }
 
@@ -70,4 +90,8 @@ internal class Settings
     internal string KeyVaultUrl { get; set; }
     internal string KeyVaultClientId { get; set; }
     internal string KeyVaultClientSecret { get; set; }
+    internal string STSAuthority { get; set; }
+    internal string STSClientId { get; set; }
+    internal string STSClientSecret { get; set; }
+    internal string STSScope { get; set; }
 }
